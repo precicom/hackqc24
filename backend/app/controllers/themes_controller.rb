a3d45222f5c3617983469f892eb9fbbf046fd920
Easymr1 <mimounr@precicom.com>
2024-03-10 15:06:02 -0400

    generate crud and enum theme

--- backend/app/controllers/themes_controller.rb
+++ backend/app/controllers/themes_controller.rb
@@ -1,2 +1,48 @@
 class ThemesController < ApplicationController
+    skip_before_action :verify_authenticity_token, only: [:index, :show]
+
+    def index
+        themes = Theme.all.order(name: :asc)
+        render json: themes, status: :ok
+    end
+
+    def create
+        themes = Theme.new(permitted_params)
+        if themes.save     
+           render json: themes, status: :created
+        else
+           render json: { error: 'Failed to create a Theme' }, 
+              status: :not_acceptable
+        end
+    end
+
+    def show
+        themes = Theme.find(params[:id])
+        render json: themes, status: :ok
+    end
+
+    def update
+        themes = Theme.find(params[:id])
+        if themes.update(permitted_params)   
+           render json: themes, status: :ok
+        else
+           render json: { error: 'Failed to update a theme' }, 
+              status: :not_acceptable
+        end
+    end
+
+    def destroy
+        themes = Theme.find(params[:id])
+        if themes.destroy
+           render json: {message: 'Theme has been desroyed'}, status: :ok
+        else
+           render json: { error: 'Failed to destroy theme' }, 
+              status: :not_acceptable
+        end
+    end
+
+    private
+    def permitted_params
+       params.permit(:name, :generated_summary, :category)
+    end
 end
--- backend/app/models/theme.rb
+++ backend/app/models/theme.rb
@@ -1,2 +1,20 @@
 class Theme < ApplicationRecord
+    enum :category, { 
+        territoire: 0,
+        soumissions: 1,
+        affaires_juridiques: 2,
+        direction_generale: 3,
+        ressources_financiere: 4,
+        greffe: 5,
+        service_police: 6,
+        securite_incendie: 7,
+        travaux_public: 8,
+        environnement: 9,
+        urbanisme: 10,
+        developpement_economique: 11,
+        loisirs_et_culture: 12,
+        legislation: 13,
+    }
+
+    
 end
