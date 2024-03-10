a7a65c35099be519878fffe95be3fac15a5a8942
jacquesmichelh <jacquesmichel.hache@precicom.com>
2024-03-10 15:43:06 -0400

    rename

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,11 @@ import { HomeComponent } from './components/home/home/home.component'; // Adjust
 import { inject } from '@angular/core';
 import { AuthGuard } from './auth/auth.guard';
 import { ThemeListComponent } from './components/theme/theme-list/theme-list.component';
-import { CouncilFicheComponent } from './components/counsil/council-fiche/council-fiche.component';
+import { CouncilFicheComponent } from './components/council/council-fiche/council-fiche.component';
 import { MobileFrameComponent } from './components/layouts/mobile-frame/mobile-frame.component';
 import { MainLayoutComponent } from './components/layouts/main-layout/main-layout.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
-import { CouncilListComponent } from './components/counsil/council-list/council-list.component';
+import { CouncilListComponent } from './components/council/council-list/council-list.component';
 
 export const routes: Routes = [
   {
