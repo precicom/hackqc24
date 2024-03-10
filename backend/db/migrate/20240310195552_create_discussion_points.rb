34fa5c95919278dc806358b2bd649b5c340d2650
Easymr1 <mimounr@precicom.com>
2024-03-10 15:57:37 -0400

    create table discussionPoints

---
+++ backend/db/migrate/20240310195552_create_discussion_points.rb
@@ -0,0 +1,11 @@
+class CreateDiscussionPoints < ActiveRecord::Migration[7.1]
+  def change
+    create_table :discussion_points do |t|
+      t.references :theme
+      t.references :council
+      t.text :generated_summary
+      t.string :minute_link_url
+      t.timestamps
+    end
+  end
+end
