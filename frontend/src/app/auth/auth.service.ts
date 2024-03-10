9d89420ffc78b8d73b4e0c42bf81ef21d044553c
jacquesmichelh <jacquesmichel.hache@precicom.com>
2024-03-10 13:11:57 -0400

    Temporary fix to preven having to relog everytime angular refreshes the page

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -7,21 +7,32 @@ import { BehaviorSubject, Observable, of, tap } from 'rxjs';
 export class AuthService {
   private isAuthenticated = new BehaviorSubject<boolean>(false);
 
-  constructor() {}
+  constructor() {  
+    // Check the authentication status from local storage on service initialization
+    const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+    this.isAuthenticated.next(isLoggedIn);
+  }
 
   login(email: string): Observable<boolean> {
     return of(true).pipe(
       tap(() => {
         this.isAuthenticated.next(true);
+
+        // Save authentication status to local storage
+        localStorage.setItem('isLoggedIn', 'true');
       })
     );
   }
 
   logout(): void {
     this.isAuthenticated.next(false);
+
+    // Remove authentication status from local storage
+    localStorage.removeItem('isLoggedIn');
   }
 
   isLoggedIn(): boolean {
     return this.isAuthenticated.value;
   }
+  
 }
