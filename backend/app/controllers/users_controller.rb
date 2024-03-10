e8d322b260489df47ba4f320f467f61cb92f0674
Easymr1 <mimounr@precicom.com>
2024-03-10 14:26:59 -0400

    update create user return token

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
     skip_before_action :verify_authenticity_token, only: [:create]
-    
+
     def create
        user = User.new(user_params)
        if user.valid? && user.save
-          token = encode_token(user_id: user.id) 
-          render json: { user: user, jwt: token }, status: :created
+        payload = {
+            user_id: user.id,
+            exp: 12.hours.from_now.to_i # Set expiration to 12 hours
+          }
+          token = JWT.encode(payload, Rails.application.secrets.secret_key_base, 'HS256')
+          render json: { token: token }, status: :created
        else
           render json: { error: 'Failed to create user' }, 
              status: :not_acceptable
