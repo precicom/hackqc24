da1a5d1b297a2f61157b37e16ab4d43a8e4eb006
Easymr1 <mimounr@precicom.com>
2024-03-11 00:33:48 -0400

    push some changes

--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ApplicationRecord
   belongs_to :theme, optional: true
 
   enum :status, { in_process: 0, accepted: 1, rejected: 2 }
+
+  has_many :comments
+  has_many :user_votes
+  
+  accepts_nested_attributes_for :comments
+  accepts_nested_attributes_for :user_votes
 end
