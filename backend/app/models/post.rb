6d84fc5c38b92843c32a880baa3a75521b2d09d6
jacquesmichelh <jacquesmichel.hache@precicom.com>
2024-03-11 00:55:27 -0400

    fix associations

--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -1,12 +1,9 @@
 class Post < ApplicationRecord
+  enum :status, { in_process: 0, accepted: 1, rejected: 2 }
+
   belongs_to :user
   belongs_to :theme, optional: true
 
   has_many :comments, dependent: :destroy
   has_many :user_votes, as: :reference, dependent: :destroy
-
-  enum :status, { in_process: 0, accepted: 1, rejected: 2 }
-
-  has_many :comments
-  has_many :user_votes
 end
