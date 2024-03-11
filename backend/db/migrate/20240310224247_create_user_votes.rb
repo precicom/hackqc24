192714fd2fb1ae8c9a82ce735426c58e05948bf0
Easymr1 <mimounr@precicom.com>
2024-03-10 23:39:04 -0400

    added relation polymorphique foir user votes

--- backend/db/migrate/20240310224247_create_user_votes.rb
+++ backend/db/migrate/20240310224247_create_user_votes.rb
@@ -2,7 +2,7 @@ class CreateUserVotes < ActiveRecord::Migration[7.1]
   def change
     create_table :user_votes do |t|
       t.references :user, null: false
-      t.references :post, null: false
+      t.references :reference, polymorphic: true, null: false
       t.boolean :is_downvote
       t.timestamps
     end
