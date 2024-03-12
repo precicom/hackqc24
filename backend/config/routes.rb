eab948987f43c6fcc76cfe5ebef827ae9367daa0
Alexis <alexisc@precicom.com>
2024-03-12 09:50:19 -0400

    fix ping http

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -27,11 +27,11 @@ Rails.application.routes.draw do
       get :me
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  post 'login', to: 'auth#create'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  post 'login', to: 'auth#create'
+  root "rails/health#show"
 end
