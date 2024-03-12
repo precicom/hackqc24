ffb6896556acf1e362de2fe9979d29bc9491ab8b
jacquesmichelh <jacquesmichel.hache@precicom.com>
2024-03-11 20:22:33 -0400

    post show

--- frontend/src/app/components/posts/post-show/post-show.component.ts
+++ frontend/src/app/components/posts/post-show/post-show.component.ts
@@ -4,13 +4,15 @@ import { Post } from '../../../repository/posts/classes';
 import { TimeDiffProPipe } from "../../../pipes/time-diff-pro/time-diff-pro.pipe";
 import { PostCommentCountPipe } from "../pipes/post-comment-count.pipe";
 import { PostUpVoteCountPipe } from "../pipes/post-up-vote-count.pipe";
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-post-show',
     standalone: true,
     templateUrl: './post-show.component.html',
     styleUrl: './post-show.component.scss',
-    imports: [TimeDiffProPipe, PostCommentCountPipe, PostUpVoteCountPipe]
+    imports: [TimeDiffProPipe, PostCommentCountPipe, PostUpVoteCountPipe, CommonModule, RouterModule]
 })
 export class PostShowComponent {
   @Input({ transform: numberAttribute }) set postId(postId: number){
@@ -19,6 +21,12 @@ export class PostShowComponent {
     })
   }
 
+  clampText = true
+
   post?: Post
   dataServices = inject(DataServices)
+
+  unClampText(){
+    this.clampText = false
+  }
 }
