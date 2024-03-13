a288b648208b5aa902fd6eb83cf625ea38c3420b
Alexis <alexisc@precicom.com>
2024-03-13 00:56:13 -0400

    add popular themes

--- backend/app/controllers/themes_controller.rb
+++ backend/app/controllers/themes_controller.rb
@@ -7,7 +7,15 @@ class ThemesController < ApplicationController
    end
 
    def popular
-      
+      theme_ids = Post.joins("LEFT OUTER JOIN comments on (posts.id = comments.post_id AND comments.created_at > '#{1.month.ago}')", "LEFT OUTER JOIN user_votes on (posts.id = user_votes.reference_id AND user_votes.reference_type = 'Post' AND user_votes.created_at > '#{1.month.ago}')")
+          .select("posts.theme_id, count(distinct comments.id) + count(distinct user_votes.id)  AS score")
+          .where("posts.created_at > :start OR comments.created_at > :start OR user_votes.created_at > :start", start: 1.month.ago)
+          .group("posts.theme_id")
+          .order("score DESC, posts.created_at DESC")
+          .map(&:theme_id)
+          
+      themes = Theme.find(theme_ids)
+      render json: themes, status: :ok
    end
 
    def create
--- frontend/src/app/repository/themes/themes-data.service.ts
+++ frontend/src/app/repository/themes/themes-data.service.ts
@@ -1,25 +1,23 @@
-import { Injectable, inject } from '@angular/core';
-import { AuthService } from '../../auth/auth.service';
-import { HttpClient } from '@angular/common/http';
-import { Observable, delay, of } from 'rxjs';
-import { Theme } from './classes';
-import { THEMES } from './mock-data';
+import { Injectable, inject } from '@angular/core'
+import { AuthService } from '../../auth/auth.service'
+import { HttpClient } from '@angular/common/http'
+import { Observable, delay, of } from 'rxjs'
+import { Theme } from './classes'
+import { THEMES } from './mock-data'
+import { environment } from '../../../environments/environment'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ThemesDataService {
-  http = inject(HttpClient);
+  http = inject(HttpClient)
 
-  getAll(): Observable<Theme[]>{
-    // return this.http.get<DiscussionPoint[]>(`${environment.apiUrl}/discussion-point`);
-
-    // mock data
-    return of(THEMES).pipe(delay(500))
+  getAll(): Observable<Theme[]> {
+    return this.http.get<Theme[]>(`${environment.apiUrl}/themes`)
   }
 
   /** Top X themes as measured by activity (comments and upvotes) */
-  popularThemes(): Observable<Theme[]>{
-    return of(THEMES.slice(0, 5)).pipe(delay(500))
+  popularThemes(): Observable<Theme[]> {
+    return this.http.get<Theme[]>(`${environment.apiUrl}/themes/popular`)
   }
 }
