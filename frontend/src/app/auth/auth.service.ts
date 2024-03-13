6a3b33ac2dd4d1738337fc099c1a387327df41d7
Alexis <alexisc@precicom.com>
2024-03-12 21:54:33 -0400

    fix jwt

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -1,66 +1,68 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
-import { BehaviorSubject, Observable, map, switchMap } from 'rxjs';
-import { environment } from '../../environments/environment';
-import { User } from '../repository/users/classes';
-import { DataServices } from '../repository/dataServices';
+import { HttpClient } from '@angular/common/http'
+import { Injectable, inject } from '@angular/core'
+import { BehaviorSubject, Observable, map, switchMap } from 'rxjs'
+import { environment } from '../../environments/environment'
+import { User } from '../repository/users/classes'
+import { DataServices } from '../repository/dataServices'
 import jwt_decode from 'jwt-decode'
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  http = inject(HttpClient);
+  http = inject(HttpClient)
   dataServices = inject(DataServices)
 
-  authToken: string  = ''
+  authToken: string = ''
   currentUser: User
   currentUserId: number
 
-  private isAuthenticated = new BehaviorSubject<boolean>(false);
+  private isAuthenticated = new BehaviorSubject<boolean>(false)
 
   constructor() {
     const token = this.getToken() ?? ''
     const isLoggedIn = Boolean(token)
 
-    const decoded = jwt_decode(token)
-    this.currentUserId = decoded['user_id']    
+    if (token) {
+      const decoded = jwt_decode(token)
+      this.currentUserId = decoded['user_id']
+    }
 
     this.authToken = token
     this.isAuthenticated.next(isLoggedIn)
   }
 
   login(email: string): Observable<boolean> {
-    return this.http.post<any>(`${environment.apiUrl}/login`, {email: email}).pipe(     
+    return this.http.post<any>(`${environment.apiUrl}/login`, { email: email }).pipe(
       switchMap(response => {
-        this.setToken(response.token) 
+        this.setToken(response.token)
 
         return this.dataServices.users.me().pipe(
           map(user => {
-            this.currentUser = user; 
+            this.currentUser = user
             this.currentUserId = user.id
 
-            this.isAuthenticated.next(true);
+            this.isAuthenticated.next(true)
 
-            return true;
-          })
+            return true
+          }),
         )
       }),
-    );
+    )
   }
 
   logout(): void {
-    this.isAuthenticated.next(false);
+    this.isAuthenticated.next(false)
 
-    this.clearToken();
+    this.clearToken()
   }
 
   isLoggedIn(): boolean {
-    return this.isAuthenticated.value;
+    return this.isAuthenticated.value
   }
 
   getToken() {
-    return localStorage.getItem('loginToken');
+    return localStorage.getItem('loginToken')
   }
 
   setToken(token: string): void {
@@ -70,6 +72,6 @@ export class AuthService {
   }
 
   clearToken() {
-    localStorage.removeItem('loginToken');
+    localStorage.removeItem('loginToken')
   }
 }
